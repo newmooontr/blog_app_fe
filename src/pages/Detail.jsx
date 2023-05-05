23e4ec180800f1191267e7bbda21b8b35205069b
newmoon <newmooon.tr@gmail.com>
2023-05-05 12:06:47 +0300

    like count added

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,11 @@
 
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Detail = () => {
+  const params = useParams();
+  console.log(params);
+  
   return (
     <div>Detail</div>
   )
